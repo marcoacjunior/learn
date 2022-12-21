501448ee49cc45f6a417b05f0b9e55a4960d2abf
MarcoJunior <mcarrarajr@uol.com.br>
2022-12-21 17:22:34 -0300

    Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,17 +1,20 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_course") 
-public class Course implements Serializable {   
+public class Course implements Serializable {      
 
 	private static final long serialVersionUID = 1L;
 
@@ -21,6 +24,9 @@ public class Course implements Serializable {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 	
 	public Course() {
 		
@@ -64,6 +70,11 @@ public class Course implements Serializable {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+	
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Front-end', 'https://www.publicdomainpictures.net/pictures/390000/velka/course-introduction.jpg', 'https://cdn-icons-png.flaticon.com/512/2000/2000887.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-07-14T03:00:00Z',1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-08-14T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-08-14T03:00:00Z',1);
