4f754263d4f962a56b11768954a3b086bbb035fc
MarcoJunior <mcarrarajr@uol.com.br>
2022-12-22 10:06:24 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Front-end'
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-07-14T03:00:00Z',1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-08-14T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-08-14T03:00:00Z',1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha Principal',1,'https://www.publicdomainpictures.net/pictures/390000/velka/course-introduction.jpg',1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas duvidas',2,'https://www.publicdomainpictures.net/pictures/390000/velka/course-introduction.jpg',2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives exclusivas prara a turma',3,'https://www.publicdomainpictures.net/pictures/390000/velka/course-introduction.jpg',0,1);
